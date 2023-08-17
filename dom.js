9725f17a5195fc3df70b8e4927c238720f8a7701
Nikhilnetha119 <nikhilkumarthumma119@gmail.com>
2023-08-17 20:28:49 +0530

    tagname

--- dom.js
+++ dom.js
@@ -1,12 +1,15 @@
 
-var items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = 'green';
-// items[1].style.fontWeight = 'bold';
-// items[0].style.fontWeight = 'bold';
-// items[2].style.fontWeight = 'bold';
-// items[3].style.fontWeight = 'bold';
+// var items = document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor = 'green';
+// // items[1].style.fontWeight = 'bold';
+// // items[0].style.fontWeight = 'bold';
+// // items[2].style.fontWeight = 'bold';
+// // items[3].style.fontWeight = 'bold';
 
-for(var i=0;i<items.length;i++)
-{
-    items[i].style.fontWeight = 'bold';
-}
+// for(var i=0;i<items.length;i++)
+// {
+//     items[i].style.fontWeight = 'bold';
+// }
+var li = document.getElementsByTagName('li');
+console.log(li);
+
